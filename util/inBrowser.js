02f11b08ce15b7205777b04418464de1b2e07ba9
Rajat Kapoor <rajat100493@gmail.com>
2018-04-05 18:00:50 +0530

    Changing inbrowser util to support electron

--- util/inBrowser.js
+++ util/inBrowser.js
@@ -2,6 +2,21 @@
 
 // Note: in iron-node window is defined - but it has window.process
 // which is not there in a real browser env
-var inBrowser = ( typeof window !== 'undefined' && !window.process );
 
-module.exports = inBrowser;
+var process = window.process;
+var inElectron = false;
+// var inNodeJS = false;
+var inBrowser = ( typeof window !== 'undefined' );
+if (typeof process !== 'undefined') {
+  if (inBrowser) {
+    inElectron = true;
+  } 
+  // else {
+  //   inNodeJS = true
+  // }
+}
+
+var returnVal = inBrowser || inElectron;
+
+
+module.exports = returnVal;
